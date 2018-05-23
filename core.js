91a4f289b3e311fed1971a98a63b1fa5484e41d4
Taylor Hanayik <hanayik@gmail.com>
2018-05-23 10:13:40 -0400

    fix palpa 16, 17 letter spacing

--- core.js
+++ core.js
@@ -1997,10 +1997,10 @@ function showPreviousPalpa16Trial() {
   // content.appendChild(img)
   var spaces = "                    "
   var textDiv = document.createElement("div")
-  textDiv.style.letterSpacing = "40px"
+  textDiv.style.letterSpacing = "70px"
   textDiv.style.textAlign = 'center'
   var p = document.createElement("p")
-  p.style.fontSize = "50px"
+  p.style.fontSize = "60px"
   var txt = document.createTextNode(palpa16Trials[t].letter1.trim() + spaces
   + palpa16Trials[t].letter2.trim() + spaces
   + palpa16Trials[t].letter3.trim() + spaces
@@ -2072,10 +2072,10 @@ function showPreviousPalpa17Trial() {
   // content.appendChild(img)
   var spaces = "                    "
   var textDiv = document.createElement("div")
-  textDiv.style.letterSpacing = "40px"
+  textDiv.style.letterSpacing = "70px"
   textDiv.style.textAlign = 'center'
   var p = document.createElement("p")
-  p.style.fontSize = "50px"
+  p.style.fontSize = "60px"
   var txt = document.createTextNode(palpa17Trials[t].letter1.trim().toUpperCase() + spaces
   + palpa17Trials[t].letter2.trim().toUpperCase() + spaces
   + palpa17Trials[t].letter3.trim().toUpperCase() + spaces
